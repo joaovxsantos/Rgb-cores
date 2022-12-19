357961e0d561c42c961c7fdc0577f09225ad5b4b
João Vitor <joaovitor55670@gmail.com>
2022-12-19 16:05:27 -0300

    third-commit

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function changeColor() {
-    
-    let Colorr = document.querySelector('.color-card')
+
+    let Color = document.querySelector('.color-card')
 
     let valor1 = document.querySelector('#input1').value
     let valor2 = document.querySelector('#input2').value
     let valor3 = document.querySelector('#input3').value
 
-    Colorr.style.backgroundColor = `rgb(${valor1}, ${valor2}, ${valor3})`
+    Color.style.backgroundColor = `rgb(${valor1}, ${valor2}, ${valor3})`
 
 }
 
-
 setInterval(changeColor, 100)
+
+
+
+
+
 
